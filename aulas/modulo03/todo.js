9506f8a853be51fbf5ac79fc08a52e1dd523109c
Heitor Augusto Melecardi do Amaral <48411053+HeitorAmaral@users.noreply.github.com>
2020-05-27 11:19:20 -0300

    Módulo 03 - Aula 05 - Excluindo Todos

--- aulas/modulo03/todo.js
+++ aulas/modulo03/todo.js
@@ -11,21 +11,40 @@ var todos = [
 function renderTodos() {
   listElement.innerHTML = "";
   for (todo of todos) {
+    var position = todos.indexOf(todo);
+
     var todoElement = document.createElement("li");
     var todoText = document.createTextNode(todo);
 
+    var linkElement = document.createElement("a");
+    linkElement.setAttribute("href", "#");
+    linkElement.setAttribute("onclick", "removeTodo(" + position + ")");
+    var linkText = document.createTextNode("Excluir");
+    linkElement.appendChild(linkText);
+
     todoElement.appendChild(todoText);
+    todoElement.appendChild(linkElement);
+
     listElement.appendChild(todoElement);
   }
 }
 
 renderTodos();
 
+buttonElement.onclick = addTodo;
+
 function addTodo() {
   var todoText = inputElement.value;
-  todos.push(todoText);
-  inputElement.value = "";
-  renderTodos();
+  if (todoText.trim() == "") {
+    alert("É necessário inserir algum valor no nome no Todo!");
+  } else {
+    todos.push(todoText);
+    inputElement.value = "";
+    renderTodos();
+  }
 }
 
-buttonElement.onclick = addTodo;
+function removeTodo(position) {
+  todos.splice(position, 1);
+  renderTodos();
+}
