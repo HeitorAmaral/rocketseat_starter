2f6d06375214da146e7a4678b2988aaa7b06a62e
Heitor Augusto Melecardi do Amaral <48411053+HeitorAmaral@users.noreply.github.com>
2020-05-27 15:29:06 -0300

    Módulo 04 - Aula 02 - Promises

---
+++ aulas/modulo04/aula02.js
@@ -0,0 +1,24 @@
+var minhaPromisse = function () {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.github.com/users/HeitorAmaral");
+    xhr.send(null);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));
+        } else {
+          reject("Erro durante a requisição!");
+        }
+      }
+    };
+  });
+};
+
+minhaPromisse()
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.warn(error);
+  });
